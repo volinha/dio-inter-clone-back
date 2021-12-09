6642a318b9c670de9cfa27c03c25d67a838137ee
Vinícius Fernandes <viniciusfernandesmct@gmail.com>
2021-12-08 22:45:38 -0300

    updated: conexão db + globalerrors + namespace express

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
+import 'express-async-errors';
 import express from 'express';
+import { createConnection } from 'typeorm';
+import { globalErrors } from './middlewares/globalErrors';
 import routes from './routes';
 
-const app = express();
-const PORT = 3333;
+createConnection().then(connection => {
+    const app = express();
+    const PORT = 3333;
 
-app.use(routes)
+    app.use(express.json())
+    app.use(routes)
 
-app.listen(PORT, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+    app.use(globalErrors)
+
+    app.listen(PORT, () => {
+        console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+    });
+}).catch((error) => {
+    console.log("Unable to connect to the database", error);
 });
